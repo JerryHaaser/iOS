b105a09a7b25ef5bbe5e746c2d63de0f713bc589
Jerry Haaser <jerry.haaser@hotmail.com>
2019-10-21 17:14:16 -0500

    set up TableViewController

--- Home Chore Tracker/Home Chore Tracker/ChoreTableViewController.swift
+++ Home Chore Tracker/Home Chore Tracker/ChoreTableViewController.swift
@@ -34,9 +34,8 @@ class ChoreTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChoreCell", for: indexPath)
 
-        // Configure the cell...
 
         return cell
     }
@@ -77,14 +76,22 @@ class ChoreTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+        if segue.identifier == "ShowChoreDetailSegue" {
+            if let choreVC = segue.destination as? ChoreDetailViewController,
+                let indexPath = tableView.indexPathForSelectedRow {
+                
+//                choreVC.chore =
+//                choreVC.choreController = 
+            }
+        }
+        
     }
-    */
+    
 
 }
